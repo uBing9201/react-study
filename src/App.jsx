7c50d7e72b3d0d1874c3a95eaeb50d721a839006
ubing <ubing0101@kakao.com>
2025-04-08 12:51:58 +0900

    props 사용방법

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import React from 'react';
 import './App.css';
 
+import ExpenseItem from './components/expenses/ExpenseItem';
+
 function App() {
-  /*
-    jsx : 리액트에서 사용하는 특수한 js문법, 태그를 그대로 쓰면 알아서 변환
-          html처럼 보이지만, 실제 html은 아닌 문법
-
-    - 규칙 :
-    1. return안에있는 태그는 반드시 하나의 태그로 묶여야 함.
-    2. 빈 태그(닫는 태그가 없는)는 반드시 /> 로 마감
-    3. 태그의 class속성은 자바스크립트 키워드 class와 겹쳐서 className으로 표기
-    4. 의미없는 부모는 <React.Fragment>로 감싸면 됨
-    5. 변수값이나 함수를 출력할 때는 {}로 감싸면 됨.
-  */
-
-  // const $h1 = React.createElement('h1', null, '안녕 리액트');
-
-  const subject = 'Vanilla Javascript!!';
+  const expenses = [
+    { id: 1, title: '냠냠치킨', price: 19000, date: new Date(2023, 6, 19) },
+    { id: 2, title: '양파', price: 5000, date: new Date(2023, 6, 20) },
+    { id: 3, title: '포도', price: 20000, date: new Date(2023, 6, 21) },
+    { id: 4, title: '오렌지', price: 15000, date: new Date(2023, 6, 22) },
+  ];
 
   return (
     <>
-      <h1 className='title'>hello react</h1>
-      <h2>React 학습 시작</h2>
-      <label htmlFor='username'>이름</label>
-      <input id='username' type='text' />
+      {expenses.map((item) => (
+        <ExpenseItem
+          key={item.id} // 반복문을 통해 같은 컴포넌트를 표현 할 때, 각각을 구분할 수 있게 해주는 props
+          title={item.title}
+          price={item.price}
+          date={item.date}
+        />
+      ))}
     </>
   );
 }
---
+++ src/components/expenses/ExpenseItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ExpenseItem.css';
+
+const ExpenseItem = ({ title, price, date }) => {
+  return (
+    <div className='expense-item'>
+      <div>{date.toLocaleString()}</div>
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>{price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
