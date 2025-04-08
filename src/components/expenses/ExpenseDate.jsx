73c70637d904c7e6e3dd92c4df84abc8b6844b2b
ubing <ubing0101@kakao.com>
2025-04-08 14:21:25 +0900

    주석제거

--- src/components/expenses/ExpenseDate.jsx
+++ src/components/expenses/ExpenseDate.jsx
@@ -6,9 +6,6 @@ const ExpenseDate = ({ date }) => {
   const month = date.getMonth();
   // padStart는 문자열의 길이를 지정하고, 난머지를 뭘로 채울지 결정
   const day = date.getDate().toString().padStart(2, '0');
-  console.log(year);
-  console.log(month);
-  console.log(day);
   return (
     <div className='expense-date'>
       <div className='expense-date__month'>{month}</div>
