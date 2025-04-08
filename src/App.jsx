a1728b37b8bfe946b8b2e4913b6b1af256c58a70
ubing <ubing0101@kakao.com>
2025-04-08 15:27:01 +0900

    지출내역 입력창 제공

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import './App.css';
 
 import ExpenseItem from './components/expenses/ExpenseItem';
-import NoName from './components/NoName';
+import NewExpense from './components/newExpense/NewExpense';
 
 function App() {
   const expenses = [
     { id: 1, title: '냠냠치킨', price: 19000, date: new Date(2023, 6, 19) },
     { id: 2, title: '양파', price: 5000, date: new Date(2023, 6, 20) },
-    { id: 3, title: '포도', price: 20000, date: new Date(2023, 6, 21) },
-    { id: 4, title: '오렌지', price: 15000, date: new Date(2023, 6, 22) },
   ];
 
   return (
     <>
+      <NewExpense />
       <div className='expenses'>
         {expenses.map((item) => (
           <ExpenseItem
