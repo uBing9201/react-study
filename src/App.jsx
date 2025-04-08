da680d03543aacf4588dd88103ff4f5b3771409a
ubing <ubing0101@kakao.com>
2025-04-08 11:13:54 +0900

    eslint, prettier 설정

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,19 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  /*
+    jsx : 리액트에서 사용하는 특수한 js문법, 태그를 그대로 쓰면 알아서 변환
+          html처럼 보이지만, 실제 html은 아닌 문법
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>첫번째 리액트 스터디</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    - 규칙 :
+    1. return안에있는 태그는 반드시 하나의 태그로 묶여야 함.
+    2. 빈 태그(닫는 태그가 없는)는 반드시 /> 로 마감
+    3. 태그의 class속성은 자바스크립트 키워드 class와 겹쳐서 className으로 표기
+    4. 의미없는 부모는 <React.Fragment>로 감싸면 됨
+    5. 변수값이나 함수를 출력할 때는 {}로 감싸면 됨.
+  */
+
+  return <h1>hello react</h1>;
 }
 
 export default App;
