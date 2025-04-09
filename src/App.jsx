4cd95b7e9785955a3c60dd37f6b3ed913c867188
ubing <ubing0101@kakao.com>
2025-04-09 16:32:45 +0900

    Users 요소 추가 기능

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,20 @@
 import React, { useState } from 'react';
-import CourseList from './components/CourseGoals/CourseList';
-import CourseInput from './components/CourseGoals/CourseInput';
-import './App.css';
+import AddUsers from './components/Users/AddUsers';
+import UserList from './components/Users/UserList';
+
+const USER_LIST = [];
 
 const App = () => {
-  // 목표 데이터들의 상태 관리 배열
-  const [goals, setGoals] = useState([]);
+  const [userList, setUserList] = useState(USER_LIST);
 
-  // CourseInput 에게 전달할 함수
-  const onAddGoal = (goal) => {
-    setGoals([...goals, goal]);
-  };
-
-  // CourseItem 에게 전달할 함수
-  const onDelGoal = (id) => {
-    // id를 통해 해당 객체를 탐색한 후 배열에서 제거
-    // for (let i = 0; i < goals.length; i++) {
-    //   if (goals[i].id === id) {
-    //     const copyGoals = [...goals];
-    //     copyGoals.splice(i, 1);
-    //     setGoals(copyGoals);
-    //   }
-    // }
-    setGoals(goals.filter((goal) => goal.id !== id));
+  const addUserHandler = (newUser) => {
+    setUserList((prev) => [...prev, newUser]);
   };
 
   return (
     <div>
-      <section id='goal-form'>
-        <CourseInput onAdd={onAddGoal} />
-      </section>
-      <section id='goals'>
-        <CourseList items={goals} onDelete={onDelGoal} />
-      </section>
+      <AddUsers onAdd={addUserHandler} />
+      <UserList list={userList} />
     </div>
   );
 };
---
+++ src/components/Users/AddUsers.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import Card from '../../UI/Card';
+import styles from './AddUsers.module.css';
+import Button from '../../UI/Button';
+
+const AddUsers = ({ onAdd }) => {
+  /*
+    이름과 나이를 입력받아서, 가입하기 버튼을 누르면 가입 처리를 해 주세요.
+    가입 처리? -> App.jsx에 있는 USER_LIST에 객체 형태로 추가.
+    */
+
+  const [userValue, setUserValue] = useState({
+    userName: '',
+    age: '',
+  });
+
+  const userSubmitHandler = (e) => {
+    e.preventDefault();
+    if (!userValue.userName.trim() || userValue.age.trim() === '') {
+      return;
+    }
+
+    onAdd(userValue);
+
+    setUserValue({
+      userName: '',
+      age: '',
+    });
+  };
+
+  const userNameChangeHandler = (e) => {
+    setUserValue((prev) => ({ ...prev, userName: e.target.value }));
+  };
+
+  const ageChangeHandler = (e) => {
+    setUserValue((prev) => ({ ...prev, age: e.target.value }));
+  };
+
+  return (
+    <Card className={styles.input}>
+      <form onSubmit={userSubmitHandler}>
+        <label htmlFor='username'>이름</label>
+        <input
+          id='username'
+          type='text'
+          onChange={userNameChangeHandler}
+          value={userValue.userName}
+        />
+        <label htmlFor='age'>나이</label>
+        <input
+          id='age'
+          type='number'
+          onChange={ageChangeHandler}
+          value={userValue.age}
+        />
+        <Button type='submit'>가입하기</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUsers;
