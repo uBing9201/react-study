5560472dcc721f7a9b1da022435d08ded1145b37
ubing <ubing0101@kakao.com>
2025-04-09 15:15:04 +0900

    css module

--- src/components/CourseGoals/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 // import './CourseInput.css';
 import styles from './CourseInput.module.css';
 import Button from '../../UI/Button';
-import './A.css';
-import './B.css';
 
 const CourseInput = ({ onAdd }) => {
   // module.css를 사용하면 css 내에서 사용한 클래스 이름을 객체화 해서 전달합니다.
+  // 여러 css 파일에 있는 클래스 이름 충돌을 더이상 신경쓰지 않아도 되며
+  // 변수명으로 파일을 구분하기 때문에 이름 짓는 거에 스트레스 받지 않아도 되며
+  // 스타일의 출처를 쉽게 파악이 가능하다는 장점이 있다
   const { 'form-control': formControl, invalid } = styles;
 
   // 입력창 상태 변수
@@ -47,7 +48,6 @@ const CourseInput = ({ onAdd }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <p className='test'>Hello Module.css~~!!</p>
       <div className={`${formControl} ${!isValid ? invalid : ''}`}>
         <label>나의 목표</label>
         <input type='text' value={enteredText} onChange={handleInput} />
