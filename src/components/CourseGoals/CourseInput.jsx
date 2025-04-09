b0bd5df51a000fd564675d09094dac0a48f44e34
ubing <ubing0101@kakao.com>
2025-04-09 14:55:03 +0900

    module.css 작성 중

--- src/components/CourseGoals/CourseInput.jsx
+++ src/components/CourseGoals/CourseInput.jsx
@@ -1,25 +1,32 @@
 import React, { useState } from 'react';
-import './CourseInput.css';
+// import './CourseInput.css';
+import styles from './CourseInput.module.css';
 import Button from '../../UI/Button';
+import './A.css';
+import './B.css';
 
 const CourseInput = ({ onAdd }) => {
-  const [enteredText, setEnteredText] = useState('');
+  // module.css를 사용하면 css 내에서 사용한 클래스 이름을 객체화 해서 전달합니다.
+  const { 'form-control': formControl, invalid } = styles;
 
-  const [isValued, setIsValued] = useState(false);
+  // 입력창 상태 변수
+  const [enteredText, setEnteredText] = useState('');
+  // 입력값이 유효한지에 대한 상태 변수
+  const [isValid, setIsValid] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // submit 전에 최종 입력값 검증.
     if (!enteredText.trim()) {
-      setIsValued(false);
+      setIsValid(false);
       return;
     }
 
     onAdd({
       id: Math.random().toString(),
       text: enteredText,
-    }); //부모에게 데이터 전잘 (함수 호출)
+    }); // 부모에게 데이터 전달 (함수 호출)
 
     // 전송이 끝나면 입력창 비우기
     setEnteredText('');
@@ -30,17 +37,20 @@ const CourseInput = ({ onAdd }) => {
 
     // 입력값 검증
     if (inputValue.trim().length > 0) {
-      setIsValued(true); // 뭐라도 작성이 되어있다면 유효하다!
-    } else setIsValued(false);
+      setIsValid(true); // 뭐라도 작성이 되었다면 유효하다!
+    } else setIsValid(false);
 
     setEnteredText(e.target.value);
   };
 
+  console.log(`isValid: ${isValid}`);
+
   return (
     <form onSubmit={handleSubmit}>
-      <div className={`form-control ${!isValued ? 'invalid' : ''}`}>
-        <label style={{ color: isValued ? 'black' : 'red' }}>나의 목표</label>
-        <input type='text' onChange={handleInput} value={enteredText} />
+      <p className='test'>Hello Module.css~~!!</p>
+      <div className={`${formControl} ${!isValid ? invalid : ''}`}>
+        <label>나의 목표</label>
+        <input type='text' value={enteredText} onChange={handleInput} />
       </div>
       <Button type='submit'>목표 추가하기</Button>
     </form>
