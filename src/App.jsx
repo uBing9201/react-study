3ca3ddb3f8d2d79070b41fed433731722cb284e3
ubing <ubing0101@kakao.com>
2025-04-10 17:38:45 +0900

    Cart Modal 열고 닫기 처리

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Food/Layout/Header';
 import AvailableMeals from './components/Food/Meals/AvailableMeals';
 import Meals from './components/Food/Meals/Meals';
+import Cart from './components/Food/Cart/Cart';
 
 const App = () => {
+  // 카트 모달의 공개 여부를 결정하는 상태 변수
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  // 모달을 열어주는 핸들러
+  const showCartHandler = () => setCartIsShown(true);
+
+  // 모달을 닫아주는 핸들러
+  const hideCartHandler = () => setCartIsShown(false);
+
   return (
-    <div>
-      <Header />
+    <>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <Meals />
-    </div>
+    </>
   );
 };
 
--- src/components/Food/Layout/Header.jsx
+++ src/components/Food/Layout/Header.jsx
@@ -3,12 +3,12 @@ import styles from './Header.module.scss';
 import mealsImage from '../../../assets/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = ({ onShowCart }) => {
   return (
     <>
       <header className={styles.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onShow={onShowCart} />
         <button>Cart</button>
       </header>
       <div className={styles['main-image']}>
